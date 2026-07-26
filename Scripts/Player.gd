0f3ae7a85fae11d43e653952025d25bc5b9f1a7f
gitcold <y1658623870@163.com>
2026-07-26 09:26:16 +0800

    Last Sync: 2026-07-26 09:26 (Mobile)

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,40 @@
 extends CharacterBody2D
 
+enum MOVE_MODE {
+	TOUCH,
+	JOYSTICK
+}
+
+@export var move_mode : MOVE_MODE = MOVE_MODE.JOYSTICK
 @export var move_speed : float = 250
-@export var move_speed_log : float = 200
+@export var move_speed_log : float = 75
 @export var bullet_scene : PackedScene
 @export var move_area_r : float = 35
+@export var joystick : VirtualJoystick
 
+func _ready() -> void:
+	match move_mode:
+		MOVE_MODE.TOUCH:
+			joystick.visible = false
+		MOVE_MODE.JOYSTICK:
+			joystick.visible = true
+	
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left", "right", "up", "down") * move_speed
-	
-	#朝向鼠标
-	var mouse_pos = get_global_mouse_position()
-	look_at(mouse_pos)
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		var local_mouse_pos = to_local(get_global_mouse_position())
-		var distance = (local_mouse_pos.x ** 2 + local_mouse_pos.y ** 2 )
-		if distance >= (move_area_r ** 2):
-			var theta = rotation
-			velocity += Vector2(cos(theta), sin(theta)) * move_speed_log * (log(distance/5000+2.8) / log(2.8))
-		
+	match move_mode:
+		MOVE_MODE.TOUCH:
+			#朝向鼠标
+			var mouse_pos = get_global_mouse_position()
+			look_at(mouse_pos)
+			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+				var local_mouse_pos = to_local(get_global_mouse_position())
+				var distance = (local_mouse_pos.x ** 2 + local_mouse_pos.y ** 2 )
+				if distance >= (move_area_r ** 2):
+					var theta = rotation
+					velocity += Vector2(cos(theta), sin(theta)) * move_speed_log * (log(distance/5000+1.3) / log(1.3))
+					#velocity += Vector2(cos(theta), sin(theta)) * 1 * distance * 0.01
+		MOVE_MODE.JOYSTICK:
+			look_at(position + velocity)
 	move_and_slide()
 	
 
@@ -29,3 +46,14 @@ func _on_fire() -> void:
 	bullet_node.direction = bullet_direction
 	bullet_node.rotation = rotation
 	get_tree().current_scene.add_child(bullet_node)
+
+
+func _on_move_mode_change() -> void:
+	match move_mode:
+		MOVE_MODE.TOUCH:
+			move_mode = MOVE_MODE.JOYSTICK
+			joystick.visible = true
+		MOVE_MODE.JOYSTICK:
+			move_mode = MOVE_MODE.TOUCH
+			joystick.visible = false
+			
